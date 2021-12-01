b9e70acca05277577ccdd3b825834989bf16af7d
Kirill Andreev <kirill.andreev@klarna.com>
2021-12-01 10:47:57 +0100

    Sonar Sweep #2

--- src/main/kotlin/SonarSweep.kt
+++ src/main/kotlin/SonarSweep.kt
@@ -1,4 +1,4 @@
-fun main() {
+fun increments() {
     val depths = readFile("SonarSweep").split("\n").map { Integer.parseInt(it) }
     var increments = 0
 
@@ -9,4 +9,24 @@ fun main() {
     }
 
     println(increments)
+}
+
+fun slidingWindowIncrements() {
+    val depths = readFile("SonarSweep").split("\n").map { Integer.parseInt(it) }
+    var increments = 0
+
+    var curSum = depths[0] + depths[1] + depths[2]
+
+    for (i in 0..(depths.size - 4)) {
+        val nextSum = curSum - depths[i] + depths[i + 3]
+        if (nextSum > curSum) {
+            increments++
+        }
+        curSum = nextSum
+    }
+    println(increments)
+}
+
+fun main() {
+    slidingWindowIncrements()
 }
