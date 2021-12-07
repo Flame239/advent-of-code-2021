6973d4498a5cb57e61dbddf9cfc2e7eb3152ee36
Kirill Andreev <kirill.andreev.239@gmail.com>
2021-12-07 11:49:29 +0100

    Treacchery of whales #2: brute force

--- src/main/kotlin/TreacheryOfWhales_7.kt
+++ src/main/kotlin/TreacheryOfWhales_7.kt
@@ -1,17 +1,31 @@
 import kotlin.math.abs
+import kotlin.math.min
 
 val crabPositions: List<Int> by lazy {
     readFile("TreacheryOfWhales").split(",").map { it.toInt() }
 }
-
 
 fun getMostEfficientCrabsPosition(): Int {
     val position = crabPositions.sorted()[crabPositions.size / 2]
     return crabPositions.sumOf { abs(position - it) }
 }
 
+fun getMostEfficientCrabsPosition2(): Int {
+    val min = crabPositions.minOrNull()!!
+    val max = crabPositions.maxOrNull()!!
+
+    var minCost = Integer.MAX_VALUE
+    for (i in min..max) {
+        val curCost = crabPositions.sumOf { arithmeticProgressionSum(abs(i - it)) }
+        minCost = min(curCost, minCost)
+
+    }
+    return minCost
+}
+
+fun arithmeticProgressionSum(n: Int): Int = n * (n + 1) / 2
+
 fun main() {
     println(getMostEfficientCrabsPosition())
-
-
+    println(getMostEfficientCrabsPosition2())
 }
