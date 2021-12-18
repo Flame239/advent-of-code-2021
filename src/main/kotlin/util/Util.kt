96c3fc3e82b2b59b520ab82f76c788d616a91538
Kirill Andreev <kirill.andreev.239@gmail.com>
2021-12-18 23:59:19 +0100

    package for util classes

--- src/main/kotlin/util/Util.kt
+++ src/main/kotlin/util/Util.kt
@@ -11,29 +11,6 @@ infix fun Int.toward(to: Int): IntProgression {
 
 fun String.sortAlphabetically() = String(toCharArray().apply { sort() })
 
-/** ¯\_(ツ)_/¯ */
-fun hexCharToBinaryString(char: Char): String {
-    return when (char) {
-        '0' -> "0000"
-        '1' -> "0001"
-        '2' -> "0010"
-        '3' -> "0011"
-        '4' -> "0100"
-        '5' -> "0101"
-        '6' -> "0110"
-        '7' -> "0111"
-        '8' -> "1000"
-        '9' -> "1001"
-        'A' -> "1010"
-        'B' -> "1011"
-        'C' -> "1100"
-        'D' -> "1101"
-        'E' -> "1110"
-        'F' -> "1111"
-        else -> error("Not a hex char")
-    }
-}
-
 fun <T> List<T>.orderedPairs(): Sequence<Pair<T, T>> = sequence {
     for (i in 0 until size - 1) {
         for (j in 0 until size - 1) {
